08dec71c88542c6c1e312fdfc51bedc9187467c8
steadfasterX <steadfasterX@users.noreply.github.com>
2025-03-11 17:30:24 +0100

    Update packages.mk
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Patches/Common/android_vendor_divested/packages.mk
+++ Patches/Common/android_vendor_divested/packages.mk
@@ -1,18 +1,5 @@
-# Replacements
-PRODUCT_PACKAGES += \
-    EtarPrebuilt \
-    FossifyGallery
-
-#ifeq ($(findstring flox,$(TARGET_PRODUCT)),)
-#PRODUCT_PACKAGES += \
-#    OpenCamera
-#endif
-
 # Extras
 PRODUCT_PACKAGES += \
     TalkBack \
 
-# Notes
-# - Available (via PrebuiltApps submodule): eSpeakNG
-# - Camera Choices: None (Camera2/Snap), OpenCamera
-# - Gallery Choices: None (AOSP/Lineage), FossifyGallery
+# Notes: all prebuilts have been moved to vendor/extendrom usage
