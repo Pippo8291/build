e46eea55a017bd876f21ed2f85d83de12e154d9b
steadfasterX <steadfasterX@users.noreply.github.com>
2025-07-07 12:20:08 +0200

    func: subj check: dont' abort if not matching
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Scripts/Common/Functions.sh
+++ Scripts/Common/Functions.sh
@@ -147,8 +147,8 @@ applyPatch() {
 			applyPatchReal "$@";
 		else
                         local psubj=$(grep Subject: "$currentWorkingPatch" | cut -d ':' -f2 | sed 's/\[PATCH\]//g' | sed -E 's/^\s+'//g)
-			git log --grep="$psubj" &>/dev/null | grep -q "$psubj"
-                        if [ $? -eq 0 ];then
+			match=$(git log --grep="$psubj" &>/dev/null | grep -c "$psubj" || true)
+                        if [ $match -gt 0 ];then
                             echo "Already applied (subject match found): $currentWorkingPatch / $psubj"
                         else
 			    if git apply --reverse --check "$@" &> /dev/null; then
