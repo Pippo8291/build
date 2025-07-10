6fe71bec1132b063ffc1405ed30898d31da622cf
steadfasterX <steadfasterX@users.noreply.github.com>
2025-07-10 21:31:13 +0200

    cve: excludes for OP msm8998
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Scripts/Common/Fix_CVE_Patchers.sh
+++ Scripts/Common/Fix_CVE_Patchers.sh
@@ -504,7 +504,12 @@ commentPatches android_kernel_oneplus_msm8998.sh \
     "CVE-2020-16166" \
     "CVE-2025-38034" \
     "CVE-2022-49980" \
-    "CVE-2024-47709"
+    "CVE-2024-47709" \
+    "CVE-2023-53103/ANY/0001.patch" \
+    "CVE-2025-37785/ANY/0001.patch" \
+    "CVE-2022-49552/ANY/0001.patch" \
+    "CVE-2025-37960/ANY/0001.patch" \
+    "CVE-2022-49545/ANY/0001.patch"
 
 commentPatches android_kernel_oneplus_sdm845.sh \
     "CVE-2024-0607/4.14/0008.patch" \
