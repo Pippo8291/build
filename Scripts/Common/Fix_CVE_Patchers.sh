0777366e7bd605442f52c66b1004871eb4d3b19f
steadfasterX <steadfasterX@users.noreply.github.com>
2025-07-16 14:40:07 +0200

    cve: exclude for OP sm8150
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Scripts/Common/Fix_CVE_Patchers.sh
+++ Scripts/Common/Fix_CVE_Patchers.sh
@@ -564,7 +564,9 @@ commentPatches android_kernel_oneplus_sm8150.sh \
     "CVE-2022-49545/ANY/0001.patch" \
     "CVE-2023-53103/ANY/0001.patch" \
     "CVE-2025-38034" \
-    "CVE-2022-49980"
+    "CVE-2022-49980" \
+    "CVE-2025-38099/ANY/0001.patch"
+    
 commentPatches android_kernel_oneplus_sm8250.sh \
     "CVE-2018-5873" \
     "CVE-2020-1749" \
