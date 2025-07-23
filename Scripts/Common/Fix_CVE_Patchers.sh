64099acbc580cae9a86be06834ce87dfea78ea28
steadfasterX <steadfasterX@users.noreply.github.com>
2025-07-23 23:28:11 +0200

    cve: excludes for google msm-4.14
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Scripts/Common/Fix_CVE_Patchers.sh
+++ Scripts/Common/Fix_CVE_Patchers.sh
@@ -314,7 +314,9 @@ commentPatches android_kernel_google_msm-4.14.sh \
     "CVE-2022-49545/ANY/0001.patch" \
     "CVE-2022-49801/ANY/0001.patch" \
     "CVE-2025-38034" \
-    "CVE-2022-49980"    
+    "CVE-2022-49980" \
+    "CVE-2025-38253/ANY/0001.patch" \
+    "CVE-2025-38099/ANY/0001.patch"
 
 commentPatches android_kernel_google_redbull.sh \
     "CVE-2018-5873" \
