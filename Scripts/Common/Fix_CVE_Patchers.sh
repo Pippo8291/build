418b67377d7c1fe820ce270feb84ba2b792498a3
flaebag <yajojo111@gmail.com>
2025-07-31 23:50:06 +0530

    Exclude CVE patches for gs201

--- Scripts/Common/Fix_CVE_Patchers.sh
+++ Scripts/Common/Fix_CVE_Patchers.sh
@@ -256,7 +256,11 @@ commentPatches android_kernel_google_gs201_private_gs-google.sh \
     "CVE-2025-37960/ANY/0001.patch" \
     "CVE-2023-53093/ANY/0001.patch" \
     "CVE-2025-38034" \
-    "CVE-2022-49980"
+    "CVE-2022-49980" \
+    "CVE-2025-38253/ANY/0001.patch" \
+    "CVE-2025-38271/ANY/0001.patch" \
+    "CVE-2025-38175/ANY/0001.patch" \
+    "CVE-2025-38099/ANY/0001.patch"
 
 commentPatches android_kernel_google_marlin.sh \
     "0001-LinuxIncrementals/3.18/3.18.0098-0099.patch" \
