9e6e3a1508561cfa9e57117de9deafda6274e5ee
steadfasterX <steadfasterX@users.noreply.github.com>
2025-08-19 13:37:40 +0200

    deblob: remove widevine sepolicies (slim)
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Scripts/Common/Deblob.sh
+++ Scripts/Common/Deblob.sh
@@ -939,7 +939,8 @@ if [ "$DOS_DEBLOBBER_REMOVE_WIDEVINE_DRM" != "false" ]; then
     echo "   |- [DEBLOB: Widevine]"
     rm -rf frameworks/av/drm/mediadrm/plugins/clearkey; #Remove ClearKey
     #rm -rf frameworks/av/drm/mediacas/plugins/clearkey; #XXX: breaks protobuf inclusion
-    find device vendor -name hal_drm_widevine.te -delete	# at least avail in device/google/gs201
+    find device vendor -type f -name hal_drm_widevine.te -delete
+	find device vendor -type f -name 'file_contexts' -exec sed -i -E 's/(.*)(hal_drm_widevine_exec)(.*)/#\1\2\3/g' {} \;
 fi
 [[ -d vendor/samsung/nodevice ]] && rm -rf vendor/samsung/nodevice;
 
