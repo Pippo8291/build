98c654aae7771e9e98867fe92431ac7a58458e08
steadfasterX <steadfasterX@users.noreply.github.com>
2025-08-19 07:15:12 +0200

    cve: exclude msm4.9
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Scripts/Common/Fix_CVE_Patchers.sh
+++ Scripts/Common/Fix_CVE_Patchers.sh
@@ -301,7 +301,8 @@ commentPatches android_kernel_google_msm-4.9.sh \
     "CVE-2022-49545/ANY/0001.patch" \
     "CVE-2022-49801/ANY/0001.patch" \
     "CVE-2025-38034" \
-    "CVE-2022-49980"
+    "CVE-2022-49980" \
+    "CVE-2025-38099/ANY/0001.patch"
 
 commentPatches android_kernel_google_msm-4.14.sh \
     "CVE-2024-56554/ANY/0001.patch" \
